81a5f47185bdf2cc829e8e2f8e594142d5106d70
jsdw14 <jsdw@icloud.com>
2023-06-10 12:40:50 +1000

    pedometer works

--- main.ts
+++ main.ts
@@ -4,27 +4,36 @@ datalogger.onLogFull(function () {
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(steps)
 })
+input.onButtonPressed(Button.B, function () {
+    datalogger.deleteLog()
+    steps = 0
+    basic.showIcon(IconNames.Skull)
+    datalogger.setColumnTitles(
+    "Time",
+    "Acceleration",
+    "Step"
+    )
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+})
 let steps = 0
-steps = 0
-datalogger.setColumnTitles(
-"Time",
-"Acceleration",
-"Step"
-)
+basic.showIcon(IconNames.Happy)
 loops.everyInterval(100, function () {
     if (input.acceleration(Dimension.Strength) > 1500) {
+        basic.showIcon(IconNames.Yes)
         datalogger.log(
-        datalogger.createCV("Time", input.runningTime()),
+        datalogger.createCV("Time", input.runningTime() / 1000),
         datalogger.createCV("Acceleration", input.acceleration(Dimension.Strength)),
         datalogger.createCV("Step", steps)
         )
         steps += 1
         basic.pause(500)
-    } else {
-        datalogger.log(
-        datalogger.createCV("Time", input.runningTime()),
-        datalogger.createCV("Acceleration", input.acceleration(Dimension.Strength)),
-        datalogger.createCV("Step", steps)
-        )
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     }
 })
